a6eb931df9d87317b544e47826ba36c0e8fa6ca5
Michael Muniz <michaelmuniz@michaels-mbp.lan>
2023-03-01 17:03:43 -0600

    Added code to retrieve any saved user input from localStorage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function () {
 
   $(".saveBtn").on("click", function() {
@@ -47,18 +48,27 @@ $(document).ready(function () {
 
  setInterval(checkHour, 1000);
 
+// Get saved data from the localStorage 
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 
- // }
-        // display current day on page
-    $("#currentDay").text(dayjs().format('dddd, MMMM, D'));
 
 
+ 
+         // display current day on page
+     $("#currentDay").text(dayjs().format('dddd, MMMM, D'));
 
-
-
-
+// Noticed im having to refresh the page to make the color markers move on the hour change
+// also when i put in more detail for the format of currentDay time. 
 
   
 });
