eb92bc772d68dcfb698e868df7415dbd009c3e47
Michael Muniz <michaelmuniz@michaels-mbp.lan>
2023-03-01 15:09:02 -0600

    added function in js file to compare hour of day and to time blocks to add or remove the class  for time block to indicate correct color code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,19 +15,42 @@ $(document).ready(function () {
 
 
 
-    function checHour() {
+    function checkHour() {
 //  get current hour to make comparison 
  var currentHour = dayjs().hour();
 
 
+ // go thru time blocks 
+ $(".time-block").each(function() {
+
+  var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+  // compare time block to current time to put apropriate color in textarea
+
+  if (blockHour< currentHour) {
+    $(this).addClass("past");
+  }
+
+  else if (blockHour === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+  }
+  else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+  }
+});
+}
 
  checkHour();
+
  setInterval(checkHour, 1000);
 
 
 
 
- }
+ // }
         // display current day on page
     $("#currentDay").text(dayjs().format('dddd, MMMM, D'));
 
